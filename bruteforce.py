f6b4a1a8293148edc1440f8b139ab1dece44fbf2
Louis de Bontin <louis2bontin@gmail.com>
2021-10-07 09:26:44 +0700

    Print plus joli

--- bruteforce.py
+++ bruteforce.py
@@ -14,17 +14,35 @@ def calculate_best_wallet(actions, capital):
                 sum_pourcentage = 0
                 for action in wallet: # Il calcule donc son ROI et l'ajoute dans ma liste de wallet
                     sum_pourcentage += action[2]
-                pourcentage = (sum_pourcentage/len(wallet))/100
+                pourcentage = (sum_pourcentage/len(wallet))
                 monney_invested = capital - new_capital
-                roi = monney_invested * pourcentage
+                roi = monney_invested * (pourcentage/100)
                 wallets.append([wallet, monney_invested, pourcentage, roi, len(wallet)])
 
     wallets.sort(key = lambda x: x[3]) # Il trie les wallet par ROI
-    return wallets[-1] # Affiche le plus rentale
+    return wallets[-1] # Retourne le plus rentale
+
 
 best_wallet = calculate_best_wallet(actions, 500)
-# print(best_wallet)
-print("Le meilleur portefeuille est composés des " + str(best_wallet[4]) + " actions suivantes : ")
+print(
+    "Le meilleur portefeuille est composés des " +
+    str(best_wallet[4]) +
+    " actions suivantes : "
+)
 for action in best_wallet[0]:
-    print(action[0] + " --> Prix : " + str(action[1]) + " Bénéfices après 2ans : " + str(action[2]))
-print("Après 2 ans, ce portefeuille raportera " + str(best_wallet[3]) + "€ avec " + str(best_wallet[1]) + "€ d'investis, soit " + str(best_wallet[2]) + "% de gains.")
+    print(
+        action[0] +
+        " --> Prix : " +
+        str(action[1]) +
+        " Bénéfices après 2ans : " +
+        str(action[2])
+    )
+print(
+    "Après 2 ans, ce portefeuille raportera " +
+    str(best_wallet[3]) +
+    "€ avec " +
+    str(best_wallet[1]) +
+    "€ d'investis, soit " +
+    str(best_wallet[2]) +
+    "% de gains."
+)
