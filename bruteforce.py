2aa771265c0a15db2e9abcee36f40c0f584ef986
Louis de Bontin <louis2bontin@gmail.com>
2021-10-14 07:58:27 +0700

    optimized fini

--- bruteforce.py
+++ bruteforce.py
@@ -1,38 +1,36 @@
 import itertools
 from actions import actions
+from time import time
+
+start_time = time()
 
 def calculate_best_wallet(actions, capital):
 
-    best_wallet = [[], 0, 0, 0, 0]
+    actions.sort(key = lambda x: x[1]) # Trie les actions pour connaître la plus chère
+    highest_price = actions[-1][1]
+
+    best_wallet = [[], 0, 0, 0]
     for action in range(0, len(actions)+1): # Les 2 premières lignes créent tous les wallets possibles (peu importe le capital)
         wallets_itertools = itertools.combinations(actions, action)
         for wallet in wallets_itertools: # Pour chaque wallet il va soustraire au capital le pris de toutes les actions comprise dans le wallet
             new_capital = capital
+            ROI_new_wallet = 0
             for action in wallet:
                 new_capital -= action[1]
-                # Peut être indenter le if, et comment on sait pour 114
-            # if new_capital < 0:
-            #     break
-            if new_capital == 0 and new_capital <114: #Si le capital restant est compris entre 0 et le priw de l'action la plus chere, il retient le wallet comme étant viable
-                sum_pourcentage = 0
+
+            if new_capital >= 0 and new_capital < highest_price: #Si le capital restant est compris entre 0 et le prix de l'action la plus chere, il retient le wallet comme étant viable
                 for action in wallet: # Il calcule donc son ROI et l'ajoute dans ma liste de wallet # Cette boucle est en 2x car sinon il ne calcule pas le %age correctement
-                    sum_pourcentage += action[2]
-                pourcentage = (sum_pourcentage/len(wallet))
+                    ROI_new_wallet += (action[2]/100)*action[1]
                 monney_invested = capital - new_capital
-                roi = monney_invested * (pourcentage/100)
                 # Une vaiable best wallet, et la comparer avec new wallet
-                if roi > best_wallet[3]:
-                    best_wallet = [wallet, monney_invested, pourcentage, roi, len(wallet)]
+                if ROI_new_wallet > best_wallet[2]:
+                    best_wallet = [wallet, monney_invested, ROI_new_wallet, len(wallet)]
 
     return best_wallet # Retourne le plus rentale
 
 
 best_wallet = calculate_best_wallet(actions, 500)
-print(
-    "Le meilleur portefeuille est composés des " +
-    str(best_wallet[4]) +
-    " actions suivantes : "
-)
+print("--- %s seconds ---" % (time() - start_time))
 for action in best_wallet[0]:
     print(
         action[0] +
@@ -42,11 +40,16 @@ for action in best_wallet[0]:
         str(action[2])
     )
 print(
-    "Après 2 ans, ce portefeuille raportera " +
+    "Le meilleur portefeuille est composés des " +
     str(best_wallet[3]) +
+    " actions ci-dessus : "
+)
+print(
+    "Après 2 ans, ce portefeuille raportera " +
+    str(best_wallet[2]) +
     "€ avec " +
     str(best_wallet[1]) +
     "€ d'investis, soit " +
-    str(best_wallet[2]) +
-    "% de gains."
+    str((best_wallet[2]/best_wallet[1])*100) +
+    "% de plus value."
 )
